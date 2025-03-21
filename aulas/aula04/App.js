43f6ba0072cdee8b7a8175d028f37a715ed1f1bb
Daniel Amorim <danielpachecoamorim@gmail.com>
2025-03-20 21:06:27 -0300

    Criando tela DetalhesProduto na aula04

--- aulas/aula04/App.js
+++ aulas/aula04/App.js
@@ -1,8 +1,9 @@
+import DetalhesProduto from "./screens/DetalhesProduto";
 import GaleriaFotos from "./screens/GaleriaFotos";
 import RegistrarConta from "./screens/RegistrarConta";
 
 function App() {
-return <GaleriaFotos />
+return <DetalhesProduto />
 
 }
 
