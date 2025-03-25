e5944a40548d0f1c499067375399d66dda864c92
Daniel Amorim <danielpachecoamorim@gmail.com>
2025-03-25 20:21:34 -0300

    Adicionando arquivos da pratica04

--- praticas/pratica04/App.js
+++ praticas/pratica04/App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
+import ContactScreen from "./screens/ContactScreen";
+import GalleryScreen from "./screens/GalleryScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import DetailScreen from "./screens/DetailScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App() {
+  return <DetailScreen/>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
