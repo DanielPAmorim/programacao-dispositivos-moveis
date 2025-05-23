a7f3f9425427418411b431b44b42add5f7d86391
Daniel Amorim <danielpachecoamorim@gmail.com>
2025-05-22 21:43:32 -0300

    Refatorei as telas e conclui o app

--- aulas/aula09/components/Grid.js
+++ aulas/aula09/components/Grid.js
@@ -1,18 +1,12 @@
+import { useContext } from "react";
 import { View, FlatList } from "react-native";
 import { Button, Text } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
-const categorias = [
-    "Restaurantes",
-    "Lanchonetes",
-    "Bares",
-    "Pizzarias",
-    "Mercados",
-    "Soverterias",
-    "Cafeterias",
-  ];
-  
+
+import { LojaContext } from "../contexts/LojaContext";
 function Grid() {
     const navigation = useNavigation();
+    const { categorias } = useContext(LojaContext);
   return (
     <View>
       <Text variant="titleMedium" style={{marginBottom:8}}>Categorias</Text>
@@ -22,8 +16,8 @@ function Grid() {
         keyExtractor={(item, index) => index}
         renderItem={({ item }) => (
           <View style={{ flex: 1, marginBottom: 8, marginRight: 8 }}>
-            <Button mode="outlined" onPress={() => navigation.navigate("Lojas", {categoria: item})}>
-              {item}
+            <Button icon= {item.icone} contentStyle={{flexDirection: "row-reverse "}} mode="outlined" onPress={() => navigation.navigate("Lojas", {categoria: item.nome})}>
+              {item.nome}
             </Button>
           </View>
         )}
--- aulas/aula09/components/ListaVertical.js
+++ aulas/aula09/components/ListaVertical.js
@@ -1,32 +1,25 @@
+import { useContext } from "react";
 import { View, FlatList } from "react-native";
 import { List, Text } from "react-native-paper";
 
-const lojas = [
-    "Restaurante Asa Sul",
-    "Restaurante Asa Norte",
-    "Restaurante Tagua",
-    "Lanchonete Sudoeste",
-    "Lanchonete Noroeste",
-    "Bar Ceilandia",
-    "Bar Nucleo",
-    "Pizzaria Sobradinho"
-];
+import { LojaContext } from "../contexts/LojaContext";
 
 function ListaVertical() {
-    return <View>
-        <Text variant="titleMedium">Lojas</Text>
-        <FlatList 
-            data={lojas}
-            showsVerticalScrollIndicator={false}
-            keyExtractor={(item, index) => index}
-            renderItem={({item}) => (
-                <List.Item
-                    title={item}
-                    description="* 4,5"               
-                />
-            )}
-        />
+  const { lojas } = useContext(LojaContext);
+
+  return (
+    <View>
+      <Text variant="titleMedium">Lojas</Text>
+      <FlatList
+        data={lojas}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item, index) => index}
+        renderItem={({ item }) => (
+          <List.Item title={item.nome} description={`* ${item.nota}`} />
+        )}
+      />
     </View>
+  );
 }
 
 export default ListaVertical;
