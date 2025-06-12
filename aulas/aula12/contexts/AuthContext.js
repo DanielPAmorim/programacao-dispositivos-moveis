65655cfb048eca97fb852129ccf3ca51801370d2
Daniel Amorim <danielpachecoamorim@gmail.com>
2025-06-12 20:15:37 -0300

    Finalizando telas da aula12

--- aulas/aula12/contexts/AuthContext.js
+++ aulas/aula12/contexts/AuthContext.js
@@ -1,41 +1,41 @@
-import {createContext, useState} from "react";
+import { createContext, useState } from "react";
 import * as AuthService from "../services/AuthService";
 
 const AuthContext = createContext();
 
-const AuthProvider = ({children}) => {
-    const [usuario,setUsuario] = useState({ login: null, logado: false});
-    const [erro,setErro] = useState("");
+const AuthProvider = ({ children }) => {
+  const [usuario, setUsuario] = useState({ login: null, logado: false });
+  const [erro, setErro] = useState("");
 
-    const login = async ({email, senha}) => {
-        try {
-            await AuthService.signIn({email, password: senha});
-            setUsuario({login: email, logado: true});
-            setErro("");
-
-        } catch (error) {
-            setErro(`Deu ruim: ${error.message}`)
-        }
+  const login = async ({ email, senha }) => {
+    try {
+      await AuthService.signIn({ email, password: senha });
+      setUsuario({ login: email, logado: true });
+      setErro("");
+    } catch (error) {
+      setErro(`Deu ruim: ${error.message}`);
     }
+  };
 
-    const logout = () =>{
-        setUsuario({login: null, logado: false})
-    };
+  const logout = () => {
+    setUsuario({ login: null, logado: false });
+  };
 
-    const registrar = async (email, senha) => {
-        try {
-            await AuthService.signUp({email, password: senha});
-            setUsuario({login: email, logado: true});
-            setErro("");
-
-        } catch (error) {
-            setErro(`Deu ruim: ${error.message}`)
-        }
+  const registrar = async ({ email, senha }) => {
+    try {
+      await AuthService.signUp({ email, password: senha });
+      setUsuario({ login: email, logado: true });
+      setErro("");
+    } catch (error) {
+      setErro(`Deu ruim: ${error.message}`);
     }
+  };
 
-    return <AuthContext.Provider value={{usuario, erro, login, logout, registrar}}>
-        {children}
+  return (
+    <AuthContext.Provider value={{ usuario, erro, login, logout, registrar }}>
+      {children}
     </AuthContext.Provider>
+  );
 };
 
-export { AuthContext, AuthProvider}
+export { AuthContext, AuthProvider };
